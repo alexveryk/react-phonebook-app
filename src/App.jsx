afb54f1653b8ae5daf29697bd025b2b1ebecc97a
Alex <48521735+alexveryk@users.noreply.github.com>
2024-03-21 12:29:51 +0000

    Added basic component markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from "react";
+import "./App.css";
+import { nanoid } from "nanoid";
 
-function App() {
-  const [count, setCount] = useState(0)
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+  contactId = nanoid(6);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  handleChange = (evt) => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    const form = evt.currentTarget;
+    const name = form.elements.name.value;
+    const contact = { id: this.contactId, name };
+
+    this.setState((prevState) => {
+      return {
+        contacts: [...prevState.contacts, contact],
+        name: "",
+      };
+    });
+    form.reset();
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form action="" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            required
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add contact</button>
+        </form>
+
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map((contact) => (
+            <li key={contact.id}>
+              {contact.id} : {contact.name}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
-
-export default App
