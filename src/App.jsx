f217a37eace94d16d5c7210802a64d286f6e8efa
Oleksandr Veryk <veryk.av@gmail.com>
2024-04-25 23:06:37 +0300

    added storage of contacts in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,25 @@ import { ContactList } from './components/ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
+
     name: '',
     filter: '',
   };
+
+  async componentDidMount() {
+    const contactsData = await localStorage.getItem('contacts');
+    const parsedContactsData = JSON.parse(contactsData);
+
+    this.setState({ contacts: parsedContactsData });
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      const contactsJSON = JSON.stringify(this.state.contacts);
+      localStorage.setItem('contacts', contactsJSON);
+    }
+  }
 
   handleChange = evt => {
     const { name, value } = evt.currentTarget;
